66f22338b216a318be6e585a8c1ae83637583bc3
jayadigital <bluezoob+jdgit@gmail.com>
2017-08-24 18:09:10 +0300

    nbus6

--- nbu/s6/files/rada_z_finstabilnosti/data.js
+++
@@ -1,7 +0,0 @@
-﻿$axure.loadCurrentPage(
-(function() {
-    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
-    var _creator = function() { return _(b,c,d,e,f,g,h,i,j,[k],l,_(m,n,o,p,q,r,s,_(),t,_(u,v,w,x,y,_(z,A,B,C),D,null,E,x,F,x,G,H,I,null,J,K,L,M,N,O,P,K),Q,_(R,_(),S,_()),T,_(),U,_(V,[])),W,_(),X,_());}; 
-var b="url",c="rada_z_finstabilnosti.html",d="generationDate",e=new Date(1503403483004.18),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i=true,j="variables",k="OnLoadVariable",l="page",m="packageId",n="ec51e6fc68c2496caec31720f19c274d",o="type",p="Axure:Page",q="name",r="Rada z finstabilnosti",s="notes",t="style",u="baseStyle",v="627587b6038d43cca051c114ac41ad32",w="pageAlignment",x="near",y="fill",z="fillType",A="solid",B="color",C=0xFFFFFFFF,D="image",E="imageHorizontalAlignment",F="imageVerticalAlignment",G="imageRepeat",H="auto",I="favicon",J="sketchFactor",K="0",L="colorStyle",M="appliedColor",N="fontName",O="Applied Font",P="borderWidth",Q="adaptiveStyles",R="a",S="b",T="interactionMap",U="diagram",V="objects",W="masters",X="objectPaths";
-return _creator();
-})());
